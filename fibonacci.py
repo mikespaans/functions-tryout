cf6b1dffe3b8f57a795cc9867299a72f79acd42d
Mike Spaans <99069233@mydavinci.nl>
2021-10-29 13:48:17 +0200

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,12 @@
-Getal1 = 0
-Getal2 = 1
+Nummer1 = 0
+Nummer2 = 1
 Doorgaan = 'doorgaan'
-
-while Doorgaan == 'doorgaan':
-    Getal2 = Getal1 + Getal2
-    Getal1 = Getal1 + Getal2
-    print (Getal2)
-    print (Getal1)
-    if Getal1 >= 1000:
-        Doorgaan ='stop'
+def FunctieSom(Getal1,Getal2):
+    while Doorgaan == 'doorgaan':
+        Getal2 = Getal1 + Getal2
+        Getal1 = Getal1 + Getal2
+        print (Getal2)
+        print (Getal1)
+        if Getal1 >= 1000:
+            Doorgaan ='stop'
+FunctieSom(Nummer1, Nummer2)
